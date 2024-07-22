dd73448da2e3510bd9e727cbf6368804f27dea15
Marvin <okongomarvin971@gmail.com>
2024-07-22 10:50:49 +0300

    Project structure

---
+++ src/Navigation/Nav.js
@@ -0,0 +1,11 @@
+import './Nav.css';
+
+function Nav() {
+  return (
+    <div>
+      Nav
+    </div>
+  )
+}
+
+export default Nav
---
+++ src/Recommended/Recommended.js
@@ -0,0 +1,11 @@
+import './Recommended.css'
+
+function Recommended() {
+  return (
+    <div>
+      Recommended
+    </div>
+  )
+}
+
+export default Recommended
