d3613e650c4dfe0f83a64b43ad9fdde56dbcecb7
Marvin <okongomarvin971@gmail.com>
2024-07-29 16:55:48 +0300

    products section

--- src/Products/Products.js
+++ src/Products/Products.js
@@ -1,11 +1,40 @@
-import './Products.css'
+import { AiFillStar } from "react-icons/ai";
+import { GiShoppingBag } from "react-icons/gi";
+
+import './Products.css';
 
 function Products() {
   return (
-    <div>
-      Products
-    </div>
-  )
+    <>
+      <section className='card-container'>
+        <section className='card'>
+          <img className="card-img" src="https://m.media-amazon.com/images/I/6125yAfsJKL._AC_UX575_.jpg" alt="Shoe" />
+          <div className='card-details'>
+            <h3 className='card-title'>Shoe</h3>
+            <section className='card-reviews'>
+              <AiFillStar className="ratings-start"/> 
+              <AiFillStar className="ratings-start"/> 
+              <AiFillStar className="ratings-start"/> 
+              <AiFillStar className="ratings-start"/>
+              <span className="total-reviews">4</span>
+            </section>
+            <section className="card-price">
+              <div className="price">
+                <del>$300</del> $200
+              </div>
+
+              <div className="bag">
+                <GiShoppingBag className="bag-icon"/>
+              </div>
+            </section>
+          </div>
+        </section>
+        
+      </section>
+
+      
+    </>
+  );
 }
 
-export default Products
+export default Products;
